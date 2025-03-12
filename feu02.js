b9853a0f5b141682b2d72360f210d3616cfd8a60
Nyzuro <marco.strs27@gmail.com>
2025-03-12 19:33:57 +0100

    feat: create an object that contains value and coodinates of the shape to find

--- feu02.js
+++ feu02.js
@@ -1,8 +1,23 @@
 const fs = require("fs");
+const shape = [];
 
 const readFile = (fileName) => {
   const contents = fs.readFileSync(fileName, "utf8");
   return contents;
+};
+
+const toFindCoordinates = (shapeToFind) => {
+  const splitedShape = shapeToFind.split("\r\n");
+  for (let i = 0; i < splitedShape.length; i++) {
+    for (let j = 0; j < splitedShape[i].length; j++) {
+      if (splitedShape[i][j] !== " ")
+        shape.push({
+          x: j,
+          y: i,
+          value: splitedShape[i][j],
+        });
+    }
+  }
 };
 
 const isValidFile = (fileName) => {
@@ -27,6 +42,8 @@ const getShapePosition = () => {
   if (!shapeToFind) return;
 
   const splitedBoard = board.split("\r\n");
+  toFindCoordinates(shapeToFind);
+  console.log(shape);
 };
 
 console.log(getShapePosition());
