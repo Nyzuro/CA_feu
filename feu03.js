46ad9d5053b325589d906cf32158780f7e2366f8
Nyzuro <marco.strs27@gmail.com>
2025-03-19 10:01:15 +0100

    feat: parse sudoku grid to make a matrice of number

--- feu03.js
+++ feu03.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
 
+const countInArray = (numbers, number) => {
+  return numbers.filter((element) => element == number).length;
+};
+
 const isValidArguments = (arguments) => {
   if (arguments.length !== 1) {
     console.error(
@@ -18,6 +22,18 @@ const isValidFile = (fileName) => {
   return fileName;
 };
 
+const isValidNumber = (number) => {
+  if (isNaN(number)) {
+    console.error("N'entrez que des nombres");
+    return false;
+  }
+  return true;
+};
+
+const lineVerification = (splitedGrid) => {};
+
+const isValidSudoku = (splitedGrid) => {};
+
 const readFile = (fileName) => {
   const contents = fs.readFileSync(fileName, "utf8");
   return contents;
@@ -28,11 +44,31 @@ const getArguments = () => {
   return arguments;
 };
 
+const parseSudoku = (sudokuGrid) => {
+  const splitedGrid = sudokuGrid.split("\r\n");
+  const parsedSudoku = [];
+  for (let i = 0; i < splitedGrid.length; i++) {
+    parsedSudoku[i] = [];
+    for (let j = 0; j < splitedGrid[i].length; j++) {
+      if (splitedGrid[i][j] !== ".") {
+        if (!isValidNumber(splitedGrid[i][j])) return;
+        parsedSudoku[i][j] = Number(splitedGrid[i][j]);
+      } else {
+        parsedSudoku[i][j] = splitedGrid[i][j];
+      }
+    }
+  }
+  return parsedSudoku;
+};
+
 const getSudokuResult = () => {
-  const argument = isValidArguments(getArguments());
-  if (!argument) return;
-  const sudoku = readFile(isValidFile(argument));
-  if (!sudoku) return;
+  const arguments = isValidArguments(getArguments());
+  if (!arguments) return;
+  const sudokuGrid = readFile(isValidFile(arguments[0]));
+  if (!sudokuGrid) return;
+
+  const parsedSudoku = parseSudoku(sudokuGrid);
+  console.log(parsedSudoku);
 };
 
 getSudokuResult();
