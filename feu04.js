6fb665c223fef4cc44ff0004074a9c50ccab173f
Nyzuro <marco.strs27@gmail.com>
2025-03-28 17:56:34 +0100

    convert the board in a 2D board, find the biggest square and replace the character and display it

--- feu04.js
+++ feu04.js
@@ -2,7 +2,7 @@ const { execSync } = require("child_process");
 
 const generateRandomBoard = () => {
   try {
-    const stdout = execSync(`node board_generator.js 17 10 5`).toString();
+    const stdout = execSync(`node board_generator.js 17 10 3`).toString();
     return stdout;
   } catch (error) {
     console.error(`exec error: ${error}`);
@@ -10,19 +10,13 @@ const generateRandomBoard = () => {
   }
 };
 
-const isPossibleSquare = (linesData) => {
-  if (linesData.length > 1) {
-    for (const line of linesData) {
-      if (line.whiteSpacesOnLine < linesData.length) {
-        linesData = linesData.slice(0, linesData.length - 1);
-        return isPossibleSquare(linesData);
-      }
-    }
-    return linesData;
-  } else return false;
+const convertTo2DArray = (board) => {
+  const twoDimensionalArray = [];
+  for (const line of board) twoDimensionalArray.push([...line]);
+  return twoDimensionalArray;
 };
 
-const findTheBiggestSquare = (board) => {
+const findAllSquares = (board) => {
   const findedSquares = [];
   for (let i = 0; i < board.length; i++)
     for (let j = 0; j < board[i].length; j++) {
@@ -51,16 +45,52 @@ const findTheBiggestSquare = (board) => {
         }
       }
     }
-  console.log(findedSquares);
+  return findedSquares;
 };
 
-const getTheBiggestSquare = () => {
+const isPossibleSquare = (linesData) => {
+  if (linesData.length > 1) {
+    for (const line of linesData) {
+      if (line.whiteSpacesOnLine < linesData.length) {
+        linesData = linesData.slice(0, linesData.length - 1);
+        return isPossibleSquare(linesData);
+      }
+    }
+    return linesData;
+  } else return false;
+};
+
+const findTheBiggestSquare = (allSquares, board) => {
+  let biggestSquare = allSquares[0];
+  for (const square of allSquares) {
+    if (square.length > biggestSquare.length) biggestSquare = square;
+  }
+
+  const result = board;
+
+  for (let i = biggestSquare.y; i < biggestSquare.length + biggestSquare.y; i++) {
+    for (let j = biggestSquare.x; j < biggestSquare.length + biggestSquare.x; j++) {
+      result[i].splice(j, 1, "o");
+    }
+  }
+  return result;
+};
+
+const displayBiggestSquare = () => {
   const randomBoard = generateRandomBoard();
   if (!randomBoard) return;
+
   const splitedBoard = randomBoard.split("\n");
   const cleanedBoard = splitedBoard.slice(1, splitedBoard.length - 1);
-  console.log(cleanedBoard);
-  findTheBiggestSquare(cleanedBoard);
+  console.log(randomBoard);
+  const board = convertTo2DArray(cleanedBoard);
+
+  const allSquares = findAllSquares(board);
+  const result = findTheBiggestSquare(allSquares, board);
+
+  for (const line of result) {
+    console.log(line.join(""));
+  }
 };
 
-getTheBiggestSquare();
+displayBiggestSquare();
