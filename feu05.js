5ad84a21306689e25b490c05b6e8f4c7201b62b9
Nyzuro <marco.strs27@gmail.com>
2025-04-03 10:50:36 +0200

    feat: read the file and make a 2d array with the map

--- feu05.js
+++ feu05.js
@@ -1,6 +1,12 @@
-const getArguments = () => {
-  const arguments = process.argv.slice(2);
-  return arguments;
+const fs = require("fs");
+
+const getGrid = (map) => {
+  const grid = [];
+  map = map.split("\n");
+  for (let i = 1; i < map.length - 1; i++) {
+    grid.push([map[i]]);
+  }
+  return grid;
 };
 
 const isValidArgument = (arguments) => {
@@ -15,8 +21,18 @@ const isValidArgument = (arguments) => {
   return arguments;
 };
 
+const getArguments = () => {
+  const arguments = process.argv.slice(2);
+  return arguments;
+};
+
 const getLabyrinthPath = () => {
   const arguments = isValidArgument(getArguments());
   if (!arguments) return;
   const argument = arguments[0];
+
+  const map = fs.readFileSync(`${argument}`, "utf-8");
+  const grid = getGrid(map);
 };
+
+getLabyrinthPath();
