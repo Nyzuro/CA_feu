bc826868d799ebd9cdf0abe2f39ec5ec28c044af
Nyzuro <marco.strs27@gmail.com>
2025-04-05 16:31:40 +0200

    feat: calculating all the neighbors for each nodes

--- feu05.js
+++ feu05.js
@@ -1,31 +1,68 @@
 const fs = require("fs");
+let grid = [];
+let openSet = [];
+let closedSet = [];
+let start = [];
+let end = [];
 
-const Spot = () => {
-  this.f = 0;
-  this.g = 0;
-  this.h = 0;
+const removeFromArray = (array, element) => {
+  for (let i = array.length; i >= 0; i--)
+    if (array[i] === element) {
+      array.splice(i, 1);
+    }
 };
+
+class Spot {
+  constructor(x, y, char) {
+    this.x = x;
+    this.y = y;
+    this.char = char;
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+    this.neighbors = [];
+
+    this.addNeighbors = function (grid) {
+      const i = this.x;
+      const j = this.y;
+      if (i < grid.length - 1) {
+        this.neighbors.push(grid[i + 1][j]);
+      }
+      if (i > 0) {
+        this.neighbors.push(grid[i - 1][j]);
+      }
+      if (j < grid[i].length - 1) {
+        this.neighbors.push(grid[i][j + 1]);
+      }
+      if (j > 0) {
+        this.neighbors.push(grid[i][j - 1]);
+      }
+    };
+  }
+}
 
 const getGrid = (map) => {
   const grid = [];
   map = map.split("\n");
   for (let i = 1; i < map.length - 1; i++) {
-    grid.push([map[i]]);
+    grid.push(map[i].split(""));
   }
   return grid;
 };
 
 const setup = (grid) => {
-  const openSet = [];
-  let start = [];
-  let end = [];
   for (let i = 0; i < grid.length; i++)
-    for (let j = 0; j < grid.length; j++) {
-      new Spot();
-      if (grid[i][j] === "1") start.push({ x: j, y: i });
-      else if (grid[i][j] === "2") end.push({ x: j, y: i });
+    for (let j = 0; j < grid[i].length; j++) {
+      const char = grid[i][j];
+      grid[i][j] = new Spot(i, j, char);
+      if (grid[i][j].char === "1") start.push(grid[i][j]);
+      else if (grid[i][j].char === "2") end.push(grid[i][j]);
     }
 
+  for (let i = 0; i < grid.length; i++)
+    for (let j = 0; j < grid[i].length; j++) {
+      grid[i][j].addNeighbors(grid);
+    }
   openSet.push(start);
 };
 
